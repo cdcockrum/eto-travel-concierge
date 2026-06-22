48c875ba61e2bf6df9236c858644300276a860ff
Christopher Cockrum <christopher@ccockrum.com>
2026-06-22 04:49:26 -0500

    Remove Analytics component from __root.tsx
    Removed the Analytics component from the main layout.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -7,7 +7,6 @@ import {
   Scripts,
 } from "@tanstack/react-router";
 import { useEffect, type ReactNode } from "react";
-import { Analytics } from "@vercel/analytics/react";
 
 import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
@@ -148,7 +147,6 @@ function RootComponent() {
           <Outlet />
         </main>
         <SiteFooter />
-        <Analytics />
       </div>
       <Toaster />
     </QueryClientProvider>
