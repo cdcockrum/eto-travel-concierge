b6e72ea452026f37183a610d8b40328090d85355
Christopher Cockrum <christopher@ccockrum.com>
2026-06-22 04:35:58 -0500

    Add Vercel Analytics component to root layout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -7,6 +7,7 @@ import {
   Scripts,
 } from "@tanstack/react-router";
 import { useEffect, type ReactNode } from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
@@ -132,6 +133,7 @@ function RootComponent() {
           <Outlet />
         </main>
         <SiteFooter />
+        <Analytics />
       </div>
       <Toaster />
     </QueryClientProvider>
